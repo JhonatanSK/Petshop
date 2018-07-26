6ccb53633d66b785396db4f004ed7b784a6e71b8
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2018-07-25 21:02:43 -0300

    Criando petshop

---
+++ script.js
@@ -0,0 +1,25 @@
+// let done= 0;
+// let tentativa= 0;
+// let button= document.querySelectorAll(".buttom");
+// let email= document.querySelectorAll(".email");
+// let password= document.querySelectorAll(".senha");
+// let mudatela= open(URL/"index2.html");
+
+alert("email: patrickcosta@gmail.com  senha:123")
+
+// if(button.onclick = tentativa==0+1){
+
+// }
+// if(tentativa= 1){
+    
+// }
+
+
+// if (email.innerHTML=="patrick@gmail.com" && password.innerHTML=="123") 
+// { done==+1
+// }
+
+// if (done!=0){ window.open("index2.html") }
+// alert("email: patrick@gmail.com");
+// alert("senha: 123");
+
